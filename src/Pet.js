78cbc76ec381bd0f7729e17ddced85e1600c0e85
Temiogundeji <yusuffogundeji@gmail.com>
2020-05-17 15:35:21 +0100

    react-initial setup

---
+++ src/Pet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Pet({ name, animal, breed }) {
+  return (
+    <div>
+      <h1>Name: {name.toUpperCase()}</h1>
+      <h2>Animal: {animal}</h2>
+      <h2>Breed: {breed}</h2>
+    </div>
+  );
+}
